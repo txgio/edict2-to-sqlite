66fccfaeab0c13d58d8228a815704969bc193d83
Tomás Godoi <tsmgodoi@gmail.com>
2016-05-20 23:19:55 -0300

    Handling some more combinations in the parser.

--- src/main/java/io/txg/Edict2ToSqliteConvert.java
+++ src/main/java/io/txg/Edict2ToSqliteConvert.java
@@ -1,7 +1,11 @@
 package io.txg;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,6 +16,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.stream.Stream;
 
+import com.mariten.kanatools.KanaAppraiser;
+import com.mariten.kanatools.KanaConverter;
+
 public class Edict2ToSqliteConvert {
 
 	private File sourceFile;
@@ -20,7 +27,8 @@ public class Edict2ToSqliteConvert {
 	private long numberOfEntries = 0;
 	private long total;
 	private long current;
-	
+	private Writer writer;
+
 	private static final String sql = "INSERT INTO dict (kanji, kana, entry, full_entry, extra) VALUES (?, ?, ?, ?, ?)";
 
 	public Edict2ToSqliteConvert(File sourceFile, String targetFilePath) {
@@ -41,7 +49,7 @@ public class Edict2ToSqliteConvert {
 			sql = "CREATE INDEX ix_kana ON dict (kana ASC)";
 			stmt.executeUpdate(sql);
 			stmt.close();
-			
+
 			stmt = connection.createStatement();
 			sql = "CREATE INDEX ix_kanji ON dict (kanji ASC)";
 			stmt.executeUpdate(sql);
@@ -52,9 +60,12 @@ public class Edict2ToSqliteConvert {
 			throw new RuntimeException(e);
 		}
 		try (Stream<String> lines = Files.lines(Paths.get(sourceFile.getAbsolutePath()), StandardCharsets.UTF_8);
-				Connection connection = DriverManager.getConnection("jdbc:sqlite:" + targetFilePath)) {
+				Connection connection = DriverManager.getConnection("jdbc:sqlite:" + targetFilePath);
+				Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("dict.txt"), "utf-8"))) {
+			connection.setAutoCommit(false);
 			try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-				lines.forEach(line -> {
+				this.writer = writer;
+				lines.forEachOrdered(line -> {
 					parseLine(preparedStatement, line);
 				});
 				System.out.println(numberOfEntries);
@@ -67,7 +78,7 @@ public class Edict2ToSqliteConvert {
 	}
 
 	private void parseLine(PreparedStatement ps, String line) {
-//		System.out.println("Line:" + line);
+		// System.out.println("Line:" + line);
 		if (!line.contains("Copyright Electronic Dictionary Research")) {
 			String[] subs = line.split(" /");
 			if (subs.length > 0) {
@@ -118,11 +129,11 @@ public class Edict2ToSqliteConvert {
 				String meaning = line.substring(line.indexOf('/') + 1, line.lastIndexOf("/Ent"));
 				// System.out.println(meaning);
 				entry.setMeaning(meaning);
-//				System.out.println(entry);
+				// System.out.println(entry);
 				insertEntry(ps, entry);
 				current++;
 				System.out.print(current + "/" + total + " (");
-				System.out.printf("%.2f", ((float)current / total) * 100);
+				System.out.printf("%.2f", ((float) current / total) * 100);
 				System.out.println(")");
 			}
 		} else {
@@ -132,42 +143,99 @@ public class Edict2ToSqliteConvert {
 
 	private void insertEntry(PreparedStatement ps, Entry entry) {
 		if (entry.getKanjis().size() > 0) {
-			for (String kanji: entry.getKanjis()) {
+			for (String kanji : entry.getKanjis()) {
 				String trimmedKanji = kanji.replaceAll("\\([a-zA-Z]*?\\)", "");
-				for (String kana: entry.getKanas()) {
+				for (String kana : entry.getKanas()) {
 					String trimmedKana = kana.replaceAll("\\([a-zA-Z]*?\\)", "");
 					if (trimmedKana.contains("(") && trimmedKana.contains(")")) {
-						String betweenParenthesis = trimmedKana.substring(trimmedKana.indexOf('(') + 1, trimmedKana.indexOf(')'));
-//						System.out.println(betweenParenthesis);
+						String betweenParenthesis = trimmedKana.substring(trimmedKana.indexOf('(') + 1,
+								trimmedKana.indexOf(')'));
+						// System.out.println(betweenParenthesis);
 						if (betweenParenthesis.contains(trimmedKanji)) {
-//							System.out.println("contains it");
 							String cleanedKana = trimmedKana.replaceAll("\\(.*?\\)", "");
-							System.out.println("Case 1");
-							insertRow(ps, trimmedKanji, cleanedKana, entry.getMeaning(), entry.getFullKanjiAndKana(), entry.getExtra());
+							if (hasKatakana(cleanedKana)) {
+								System.out.println("Case 4");
+								insertRow(ps, convertKatakanaToHiragana(trimmedKanji),
+										convertKatakanaToHiragana(cleanedKana),
+										trimmedKanji + " [" + cleanedKana + "] /" + entry.getMeaning() + "/",
+										entry.getFullKanjiAndKana(), entry.getExtra());
+							} else {
+								System.out.println("Case 1");
+								insertRow(ps, trimmedKanji, cleanedKana, entry.getMeaning(),
+										entry.getFullKanjiAndKana(), entry.getExtra());
+							}
 						}
 					} else {
-						System.out.println("Case 2");
-						insertRow(ps, trimmedKanji, trimmedKana, entry.getMeaning(), entry.getFullKanjiAndKana(), entry.getExtra());
+						if (hasKatakana(trimmedKana)) {
+							System.out.println("Case 5");
+							insertRow(ps, convertKatakanaToHiragana(trimmedKanji),
+									convertKatakanaToHiragana(trimmedKana),
+									trimmedKanji + " [" + trimmedKana + "] /" + entry.getMeaning() + "/",
+									entry.getFullKanjiAndKana(), entry.getExtra());
+						} else {
+							System.out.println("Case 2");
+							insertRow(ps, trimmedKanji, trimmedKana, entry.getMeaning(), entry.getFullKanjiAndKana(),
+									entry.getExtra());
+						}
 					}
 				}
 			}
 		} else {
-			for (String kana: entry.getKanas()) {
+			for (String kana : entry.getKanas()) {
 				String trimmedKana = kana.replaceAll("\\(.*?\\)", "");
-				System.out.println("Case 3");
-				insertRow(ps, null, trimmedKana, entry.getMeaning(), entry.getFullKanjiAndKana(), entry.getExtra());
+				if (hasKatakana(trimmedKana)) {
+					System.out.println("Case 6");
+					insertRow(ps, null, convertKatakanaToHiragana(trimmedKana),
+							trimmedKana + " /" + entry.getMeaning() + "/", entry.getFullKanjiAndKana(),
+							entry.getExtra());
+				} else {
+					System.out.println("Case 3");
+					insertRow(ps, null, trimmedKana, entry.getMeaning(), entry.getFullKanjiAndKana(), entry.getExtra());
+				}
 			}
 		}
 	}
 
+	private String convertKatakanaToHiragana(String kana) {
+		return KanaConverter.convertKana(kana, KanaConverter.OP_ZEN_KATA_TO_ZEN_HIRA);
+	}
+
+	private boolean hasKatakana(String trimmedKana) {
+		for (char c : trimmedKana.toCharArray()) {
+			System.out.println(c);
+			if (KanaAppraiser.isZenkakuKatakana(c)) {
+				System.out.println("is katakana");
+				return true;
+			}
+		}
+		return false;
+	}
+
 	private void insertRow(PreparedStatement ps, String kanji, String kana, String meaning, String full, String extra) {
+		System.out.println(meaning);
+		// if (writer != null) {
+		// try {
+		// if (kanji != null) writer.write(kanji);
+		// writer.write('\t');
+		// if (kana != null) writer.write(kana);
+		// writer.write('\t');
+		// if (meaning != null)writer.write(meaning);
+		// writer.write('\t');
+		// if (full != null) writer.write(full);
+		// writer.write('\t');
+		// if (extra != null)writer.write(extra);
+		// writer.write('\n');
+		// } catch (IOException e) {
+		// throw new RuntimeException(e);
+		// }
+		// }
 		try {
 			ps.setString(1, kanji);
 			ps.setString(2, kana);
 			ps.setString(3, meaning);
 			ps.setString(4, full);
 			ps.setString(5, extra);
-			
+
 			ps.executeUpdate();
 			ps.clearParameters();
 		} catch (SQLException e) {
